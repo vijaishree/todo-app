7a4b0ffd16bcd0f2d888c54df1437f2906a42c73
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2024-05-05 14:58:21 +0530

    alters allignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import TodoApp from "./Todo-app.js";
-
+import "./styles.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
